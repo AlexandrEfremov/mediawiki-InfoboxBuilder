4db90f799898e6595dca192db162d19f69fbe2e6
Alexandr <admin@wikimultia.org>
2020-04-05 15:21:27 +0300

    Create InfoboxBuilder.i18n.magic.php

---
+++ InfoboxBuilder.i18n.magic.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Magic word translations
+ */
+
+$magicWords = [];
+
+/**
+ * English
+ * @author Adam Karmiński <adamk@wikia-inc.com>
+ */
+
+$magicWords[ 'en' ] = [
+	'infoboxbuilder' => [ 0, 'infoboxbuilder' ]
+];
